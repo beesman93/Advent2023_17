839b42ea5b0f094f90946b774c649bb0fc088e25
beesman93 <chvojtomas@gmail.com>
2023-12-17 11:41:13 +0100

    equitable struct, bad performance test

--- Program.cs
+++ Program.cs
@@ -2,70 +2,53 @@
 using System.Drawing;
 using System.Runtime.InteropServices;
 
-List<string> lines = new();
+List<List<int>> map = new() { new() };
 using (StreamReader reader = new(args[0]))
 {
     while (!reader.EndOfStream)
-        lines.Add(reader.ReadLine());
-}
-
-int ROWS = lines.Count;
-int COLS = lines[0].Length;
-
-int[][]map = new int[ROWS][];
-int[][] costFromOrigin = new int[ROWS][];
-for (int row = 0; row < ROWS; row++)
-{
-    map[row] = new int[COLS];
-    costFromOrigin[row] = new int[COLS];
-    for (int col = 0; col < COLS; col++)
     {
-        map[row][col] = lines[row][col] - '0';
-        costFromOrigin[row][col] = int.MaxValue;
+        int c = reader.Read();
+        if (c == '\n')
+            map.Add(new());
+        else if(c>='0'&&c<='9')
+            map.Last().Add(c - '0');
     }
 }
+int ROWS = map.Count;
+int COLS = map.First().Count;
 
-int part1 = solve(false);
-int part2 = solve(true);
-Console.WriteLine($"{part1}\n{part2}");
-int solve(bool part2)
+solve(false);//part1
+solve(true);//part2
+
+void solve(bool part2)
 {
     Stopwatch sw = Stopwatch.StartNew();
-    //bestTravel for each node from each dirrection with dirCount
-    //pQ of where we could travel next
-    Dictionary<(int row,int col,DIR dir,int dirCount), int> bestTravels = new();
-    PriorityQueue<(int row, int col, DIR dir, int dirCount), int> pQ = new();
-    pQ.Enqueue((0, 0, DIR.NONE, 0), 0); //(row,col,dirrection, dir_count), travel_time
-
+    Dictionary<node, int> bestTravels = new();
+    PriorityQueue<node, int> pQ = new();
+    pQ.Enqueue(new(0, 0, DIR.NONE, 0), 0);
     while (pQ.Count > 0)
     {
         pQ.TryDequeue(out var weAt,out int travel_time);
-
         if (bestTravels.ContainsKey(weAt))
             continue; //we found better before -- new path dequeued
-
         bestTravels[weAt] = travel_time;
-
         //QUEUE new paths forward:
         HashSet<DIR> newDirrections = new() { DIR.UP, DIR.DOWN, DIR.LEFT, DIR.RIGHT };
-        if(weAt.dir!=DIR.NONE)newDirrections.Remove(reverse(weAt.dir));//can't travel backwards
-        //if (part2 && weAt.Dir != DIR.NONE && weAt.SameDir < 4) newDirrections = new() { weAt.Dir };//we have to travel same dirrection in part2
-        if (weAt.dirCount >= (part2?10:3)) newDirrections.Remove(weAt.dir);//can't travel this dirrection anymore
+        newDirrections.Remove(reverse(weAt.Dir));//can't travel backwards
+        if (weAt.DirCount >= (part2?10:3)) newDirrections.Remove(weAt.Dir);//can't travel this dirrection anymore
         foreach (DIR dir in newDirrections)
         {
-            int travelHowMuch = 1;
-            if (part2 && dir != weAt.dir)
-                travelHowMuch = 4;
-            int row = weAt.row + rowModifier(dir,travelHowMuch);
-            int col = weAt.col + colModifier(dir,travelHowMuch);
+            int travelHowMuch = (part2 && dir != weAt.Dir) ? 4 : 1;
+            int row = weAt.Row + rowModifier(dir,travelHowMuch);
+            int col = weAt.Col + colModifier(dir,travelHowMuch);
             if (row >= 0 && row < ROWS && col >= 0 && col < COLS)
             {
-                int sameDir = dir == weAt.dir ? weAt.dirCount + travelHowMuch : travelHowMuch;
+                int sameDir = (dir == weAt.Dir) ? weAt.DirCount + travelHowMuch : travelHowMuch;
                 int costOfTravel = travel_time;
                 for(int i=1;i<=travelHowMuch;i++)
                 {
-                    int rowAddition = weAt.row + rowModifier(dir, i);
-                    int colAddition = weAt.col + colModifier(dir, i);
+                    int rowAddition = weAt.Row + rowModifier(dir, i);
+                    int colAddition = weAt.Col + colModifier(dir, i);
                     costOfTravel += map[rowAddition][colAddition];
                 }
                 pQ.Enqueue(new(row, col, dir, sameDir), costOfTravel);
@@ -74,11 +57,10 @@ int solve(bool part2)
     }
     int min = int.MaxValue;
     foreach (var bestTravel in bestTravels)
-        if (bestTravel.Key.row == ROWS - 1 && bestTravel.Key.col == COLS - 1)
+        if (bestTravel.Key.Row == ROWS - 1 && bestTravel.Key.Col == COLS - 1)
             if (bestTravel.Value < min)
                 min = bestTravel.Value;
-    Console.WriteLine($"part {(part2?2:1)} in {sw.Elapsed}");
-    return min;
+    Console.WriteLine($"part {(part2?2:1)}: {min}\t\t~{sw.ElapsedMilliseconds}ms");
 }
 
 int rowModifier(DIR dir,int val)
@@ -118,13 +100,29 @@ enum DIR : short
     NONE = 5
 }
 
-/* used tuple instead
-struct node
+readonly struct node : IEquatable<node>
 {
-    public int Row;
-    public int Col;
-    public DIR Dir;
-    public int SameDir;
-    public node(int row, int col, DIR d, int sameDir) =>
-        (Row, Col, Dir, SameDir) = (row, col, d, sameDir);
-}*/
+    private readonly int _row;
+    private readonly int _col;
+    private readonly DIR _dir;
+    private readonly int _dirCount;
+    public node(int row, int col, DIR dir, int dirCount) =>
+        (_row, _col, _dir, _dirCount) = (row, col, dir, dirCount);
+    public int Row => _row;
+    public int Col => _col;
+    public DIR Dir => _dir;
+    public int DirCount => _dirCount;
+
+    public bool Equals(node other)
+    {
+        if(_row!=other._row)
+            return false;
+        if(_col != other._col)
+            return false;
+        if (_dir != other._dir)
+            return false;
+        if (_dirCount != other._dirCount)
+            return false;
+        return true;
+    }
+}
